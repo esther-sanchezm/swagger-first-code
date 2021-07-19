8d49152714d8012f91c340eb624c9637b0fb6bce
esther.sanchezm <Esther.Sanch9z@gmail.com>
2021-07-19 21:50:45 +0200

    Add some funny description to show api info configuration

--- src/main/java/com/inquisitor/strategy/swagger/design/config/SwaggerConfig.java
+++ src/main/java/com/inquisitor/strategy/swagger/design/config/SwaggerConfig.java
@@ -4,10 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -19,7 +23,18 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.inquisitor.strategy.swagger.design"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Rest API of board game",
+                "Creepy description of API Rest",
+                "0.0.1-SNAPSHOT",
+                "Please, use for do something util",
+                new Contact("Invented", "www.example.com", "invented@easy.com"),
+                "License of API", "API license URL", Collections.emptyList());
     }
 
 }
